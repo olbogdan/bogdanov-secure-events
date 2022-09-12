b3a1c27ac244f5cc9ed9fe5c3a630c736aa1fe23
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-09-13 01:19:24 +0200

    convert build.gradle to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("dagger.hilt.android.plugin")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = 33
+    defaultConfig {
+        applicationId = "ch.protonmail.android.protonmailtest"
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    namespace = "ch.protonmail.android.protonmailtest"
+}
+
+//todo: extract dependencies into separate Dependencies.kt and Version.kt files
+dependencies {
+    // Android
+    implementation("androidx.fragment:fragment-ktx:1.5.2")
+    implementation("com.google.android.material:material:1.6.1")
+
+    // Networking
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+
+    // DI
+    kapt("com.google.dagger:hilt-android-compiler:2.42")
+    implementation("com.google.dagger:hilt-android:2.42")
+
+    // Cryptography
+    implementation(files("../libs/crypto.aar"))
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+buildscript {
+    val kotlinVersion = "1.7.10"
+    extra["kotlin_version"] = kotlinVersion
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
